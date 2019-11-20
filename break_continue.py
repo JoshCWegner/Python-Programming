9e57f6b33fc2b3571297a0cea4b46989daf689f0
Josh123 <wegnerjosh@gmail.com>
2019-11-20 12:59:59 -0600

    Python Commit 11.19.19

---
+++ break_continue.py
@@ -0,0 +1,15 @@
+student_names = ["James","Katarina","Jessica","Mark","Bort","Frank Grimes"]
+
+#for name in student_names:
+ #   if name == "Mark":
+  #      print("Found him! " + name)
+   #     break
+   # print("Currnetly testing " + name)
+
+
+for name in student_names:
+    if name == "Bort":
+        continue
+        print("Found him! " + name)
+        break
+    print("Currnetly testing " + name)
